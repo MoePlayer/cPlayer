b03a807014263ff35648494b52e884312f3422cf
journey-ad <journey.adc@gmail.com>
2017-01-15 21:49:43 +0800

    preload->false √

--- demo/cplayer.js
+++ demo/cplayer.js
@@ -952,5 +952,5 @@ var cContext = function () {
 	return cContext;
 }();
 if (window) window.cPlayer = cPlayer;
-console.log("\n%ccPlayer%cv2.4.2%c\n\n", "padding:7px;background:#cd3e45;font-family:'Sitka Heading';font-weight:bold;font-size:large;color:white", "padding:7px;background:#ff5450;font-family:'Sitka Text';font-style:italic;font-size:large;color:#eee", "");
+console.log("\n%ccPlayer%cv2.4.3%c\n\n", "padding:7px;background:#cd3e45;font-family:'Sitka Heading';font-weight:bold;font-size:large;color:white", "padding:7px;background:#ff5450;font-family:'Sitka Text';font-style:italic;font-size:large;color:#eee", "");
 //# sourceMappingURL=cplayer.js.map
